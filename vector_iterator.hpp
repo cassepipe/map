f5457f39ecb862a1b5b8bf70e4d930a3fbd4c0cf
cassepipe <cassepipe@ymail.com>
2022-07-07 15:38:42 +0200

    vector_iterator seems done

--- vector_iterator.hpp
+++ vector_iterator.hpp
@@ -1,24 +1,39 @@
-/* ############################ VECTOR ITERATOR ############################# */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_iterator.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cassepipe <norminet@42.fr>                 +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 16:10:39 by cassepipe         #+#    #+#             */
+/*   Updated: 2022/07/06 16:10:39 by cassepipe        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_ITERATOR_HPP
+#define VECTOR_ITERATOR_HPP
+
+#include <cstdlib>
+#include "iterator_traits.hpp"
 
 namespace ft
 {
 template <typename T> class vector_iterator
 {
   protected:
-	T* current_;
+	T*    current_;
 
   public:
 	/// EXPOSED TYPES
-	//
+	
 	// It is expected that iterators expose some traits for the purpose of using alogrithms that operates on them
 	// They are pointers with metadata in a sort
-	// We are not using iterator_traits here because T is supposed to be a pointer
-	// and not an iterator, no need for forwarding anything
-	typedef ft::random_access_iterator_tag iterator_category;
-	typedef std::ptrdiff_t difference_type;
-	typedef T value_type;
-	typedef T* pointer;
-	typedef T& reference;
+	typedef ft::random_access_iterator_tag                    iterator_category;
+	//Why ptrdiff_t ? Because it is the signed equivalent of size_t. No, ssize_t is not.
+	typedef std::ptrdiff_t                                      difference_type;
+	typedef T                                                        value_type;
+	typedef T*                                                          pointer;
+	typedef T&                                                        reference;
 
 	/// CONSTRUCTORS
 
@@ -27,49 +42,73 @@ template <typename T> class vector_iterator
 	/*Defautl Ctor*/ vector_iterator() : current_(NULL)
 	{
 	}
-	/*Value Ctor*/ vector_iterator(T* ptr) : current_(ptr)
+	
+	/*Value Ctor*/ vector_iterator(pointer ptr) : current_(ptr)
 	{
 	}
 
-	// Compiler will generate copy ctor and operator=()
+	// Compiler will generate trivial copy ctor and operator=()
+	
+	// CONVERSION OPERATOR
+	// Allow non-const to const implicit conversion !!!
+	operator vector_iterator<const T>() const
+	{
+		return (vector_iterator<const T>(current_));
+	}
+	
+	/// DEREFERENCE OPERATORS 
+	//	Basic requirements
+	
+	reference operator*() const
+	{
+		return *current_;
+	}
 
-	/// ARITHMETIC OPERATORS
+	pointer operator->() const // This one I don't understand
+	{
+		return current_;
+	}
+
+	// Random access requirement
+	reference operator[](int i) const
+	{
+		return (current_ + i);
+	}
+	
+	/// INCREMENT OPERATORS
+	// Forward Iterator requirements
 
 	vector_iterator& operator++()
 	{
-		++(current_);
-	}
-
-	vector_iterator& operator--()
-	{
-		--(current_);
+		++current_;
+		return *this;
 	}
 
 	vector_iterator& operator++(int)
 	{
 		vector_iterator tmp = *this;
-
-		++(current_);
+		++current_;
 		return tmp;
+	}
+
+	// DECREMENT OPERATORS
+	// Bidrectional iterator requirements
+
+	vector_iterator& operator--()
+	{
+		--current_;
+		return *this;
 	}
 
 	vector_iterator& operator--(int)
 	{
 		vector_iterator tmp = *this;
-
-		--(current_);
+		--current_;
 		return tmp;
 	}
 
-	vector_iterator operator+(const vector_iterator& rhs) const
-	{
-		return vector_iterator(current_ + rhs.current_);
-	}
-
-	vector_iterator operator-(const vector_iterator& rhs) const
-	{
-		return vector_iterator(current_ - rhs.current_);
-	}
+	/// ARITHMETIC OPERATORS
+	// Random access iterator requirements
 
 	vector_iterator operator+(difference_type i) const
 	{
@@ -81,20 +120,17 @@ template <typename T> class vector_iterator
 		return vector_iterator(current_ - i);
 	}
 
-	vector_iterator operator+=(const vector_iterator& rhs)
+	//Pas le choix si tu veux faire marcher des expressions telles que (-3 -it)
+	friend vector_iterator operator+(difference_type i, const vector_iterator& it)
 	{
-		current_ += rhs.current_;
-
-		return vector_iterator(current_);
+		return vector_iterator(it.current__ + i);
 	}
 
-	vector_iterator operator-=(const vector_iterator& rhs)
+	friend vector_iterator operator-(difference_type i, const vector_iterator& it)
 	{
-		current_ -= rhs.current_;
-
-		return vector_iterator(current_);
+		return vector_iterator(it.current__ - i);
 	}
-
+ 
 	vector_iterator operator+=(difference_type i)
 	{
 		current_ += i;
@@ -109,56 +145,44 @@ template <typename T> class vector_iterator
 		return vector_iterator(current_);
 	}
 
-	/// DEREFERENCE OPERATORS
-
-	T& operator*() const
+	//Allow for const to non const comparisons
+	template<typename RightIterator, typename LeftIterator>
+	friend bool operator==(const vector_iterator<RightIterator>& lhs, const vector_iterator<LeftIterator>& rhs)
 	{
-		return *current_;
+		return lhs.current_ == rhs.current_;
 	}
 
-	T* operator->() const // This one I don't understand
+	template<typename RightIterator, typename LeftIterator>
+	friend bool operator<(const vector_iterator<RightIterator>& lhs, const vector_iterator<LeftIterator>& rhs)
 	{
-		return current_;
+		return lhs.current_ < rhs.current_;
 	}
 
-	T& operator[](int i) const
+	template<typename RightIterator, typename LeftIterator>
+	friend bool operator>(const vector_iterator<RightIterator>& lhs, const vector_iterator<LeftIterator>& rhs)
 	{
-		return (current_ + i);
-	}
-
-	/// LOGICAL OPERATORS
-
-	bool operator!() const
-	{
-		return !current_;
-	}
-
-	/// RELATIONAL OPERATORS
-
-	bool operator==(const vector_iterator& rhs) const
-	{
-		return (current_ == rhs.current_);
-	}
-
-	bool operator<(const vector_iterator& rhs) const
-	{
-		return (current_ < rhs.current_);
-	}
-
-	bool operator>(const vector_iterator& rhs) const
-	{
-		return (current_ > rhs.current_);
-	}
-
-	bool operator>=(const vector_iterator& rhs) const
-	{
-		return (current_ >= rhs.current_);
-	}
-
-	bool operator<=(const vector_iterator& rhs) const
-	{
-		return (current_ >= rhs.current_);
+		return lhs.current_ > rhs.current_;
 	}
 };
+
+template<typename RightIterator, typename LeftIterator>
+bool operator!=(const vector_iterator<RightIterator>& lhs, const vector_iterator<LeftIterator>& rhs)
+{
+	return !(lhs == rhs);
+}
+
+template<typename RightIterator, typename LeftIterator>
+bool operator>=(const vector_iterator<RightIterator>& lhs, const vector_iterator<LeftIterator>& rhs)
+{
+	return !(lhs < rhs);
+}
+
+template<typename RightIterator, typename LeftIterator>
+bool operator<=(const vector_iterator<RightIterator>& lhs, const vector_iterator<LeftIterator>& rhs)
+{
+	return !(lhs > rhs);
+}
+
 } //namespace ft
 
+#endif /* VECTOR_ITERATOR_HPP */
