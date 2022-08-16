f1ee78c84004114aca1f2ad140ba9f4260c30e4f
cassepipe <cassepipe@ymail.com>
2022-08-16 23:40:27 +0200

    Compiles with const and non-const iterators

--- aa_tree.hpp
+++ aa_tree.hpp
@@ -17,9 +17,6 @@ template <typename Key, typename Value, typename KeyCmpFn = std::less<Key>,
           typename Alloc = std::allocator<std::pair<const Key, Value> > >
 class AA_tree
 {
-  public:
-	class iterator;
-
   protected:
 	struct AA_base_node;
 	struct AA_node;
@@ -49,6 +46,7 @@ class AA_tree
 		{ }
 
 	  protected:
+
 		/*Constructor*/ AA_base_node(AA_node *left, AA_node *right, int lvl) :
 			left(left),
 			right(right),
@@ -58,32 +56,41 @@ class AA_tree
 
 	struct AA_node : public AA_base_node
 	{
-		Key   key;
-		Value value;
+		Key      key;
+		Value    value;
 		AA_node *parent;
 
-		/*Default ctor*/ AA_node() : AA_base_node(NIL, NIL, 1), key(Key()), value(Value())
-		{
-		}
+		/*Default ctor*/ AA_node() :
+			AA_base_node(NIL, NIL, 1),
+			key(Key()),
+			value(Value())
+		{ }
 
-		/*Constructor*/ AA_node(Key k, Value v, AA_node *p) : AA_base_node(NIL, NIL, 1), key(k), value(v), parent(p)
-		{
-		}
+		/*Constructor*/ AA_node(Key k, Value v, AA_node *p) :
+			AA_base_node(NIL, NIL, 1),
+			key(k),
+			value(v), parent(p)
+		{ }
 	};
 
+  public:
+
+	class iterator;
+
+	typedef Key            key_type;
+	typedef Value          mapped_type;
 	typedef AA_node        node_type;
 	typedef AA_node *      node_pointer;
 	typedef std::size_t    size_type;
-	typedef Key            key_type;
-	typedef Value          mapped_type;
-	typedef typename AA_tree<Key, const Value, KeyCmpFn, Alloc>::iterator const_iterator;
 
 	// the template keyword is only here so that the < can be correctly parsed
 	typedef typename Alloc::template rebind<node_type>::other node_allocator_type;
 	// Were we able to use c++11, we would have used std::allocator_traits like this
 	// typedef typename std::allocator_traits<Alloc>::template rebind_alloc<node_type>		node_allocator_type;
+	
+  protected:
 
-	/*DATA*/
+	/* DATA */
 	node_pointer        root_;
 	size_type           size_;
 	node_allocator_type node_alloc_;
@@ -264,8 +271,7 @@ class AA_tree
 		root_(NIL),
 		size_(0),
 		node_alloc_(alloc) // node_alloc_ and alloc are different types, implicit conversion thanks to allocator's special ctor
-	{
-	}
+	{ }
 
 	/*Destructor*/ ~AA_tree()
 	{
@@ -287,58 +293,139 @@ class AA_tree
 		root_ = remove_(k, root_);
 	}
 
-	/* DEBUG */
-
-	void print_node(std::stringstream &ss, node_pointer node)
-	{
-		if (node != NIL)
-		{
-			ss << node->key << " [label=< <b>" << node->key << "</b><br/> <sub>" << node->level << "</sub>>]\n\t";
-			if (node->left != NIL)
-			{
-				if (node->left->level == node->level)
-				{
-					ss << "{rank=same; " << node->key << "; " << node->left->key << "}\n\t";
-					ss << node->left->key << " [color=red]\n\t";
-				}
-				ss << node->key << " -> " << node->left->key << "\n\t";
-				print_node(ss, node->left);
-			}
-			if (node->right != NIL)
-			{
-				if (node->right->level == node->level)
-				{
-					ss << "{rank=same; " << node->key << "; " << node->right->key << "}\n\t";
-					ss << node->right->key << " [color=red]\n\t";
-				}
-				ss << node->key << " -> " << node->right->key << "\n\t";
-				print_node(ss, node->right);
-			}
-		}
-	}
-
-#define DEFAULT_FILENAME "tree"
-	void print_dot()
-	{
-		static int n;
-
-		print_dot(++n);
-	}
-
-	void print_dot(int n)
-	{
-		std::stringstream ss;
-		print_node(ss, root_);
-
-		std::stringstream title;
-		title << DEFAULT_FILENAME << n << ".dot";
-		std::ofstream file(title.str().c_str());
-		file << "digraph {\n\n\t";
-		file << ss.str();
-		file << "\n}";
-	}
-
 	/* ITERATOR */
+
+	class const_iterator
+	{
+	  protected:
+		/* STATE */
+		node_pointer             root_;
+		node_pointer             current_;
+
+		/* HELPERS */
+
+		node_pointer leftmost_(node_pointer node) const
+		{
+			while (node->left != NIL)
+				node = node->left;
+			return node;
+		}
+
+		node_pointer rightmost_(node_pointer node) const
+		{
+			while (node->right != NIL)
+				node = node->right;
+			return node;
+		}
+
+	  public:
+
+		/* Constructor */ const_iterator(node_pointer root, node_pointer current = NULL)
+			: root_(root), current_(current)
+		{ }
+
+		/* Copy Constructor */ const_iterator(const_iterator const &other)
+			: root_(other.current_), current_(other.current_)
+		{ }
+
+		const_iterator &operator=(const_iterator const &rhs)
+		{
+			current_ = rhs.current_;
+			root_ = rhs.root_;
+			return *this;
+		}
+
+		// const_iterator will cycle forward passing through an end's marker
+		const_iterator &operator++()
+		{
+			if (root_ == NIL) // Tree empty ?
+				current_ = NULL;
+			else if (current_ == NULL) 
+				current_ = leftmost_(root_);
+			// If tree was empty but stuff got in since last call
+			 // If has successor...
+			else if (current_->right != NIL)
+				current_ = leftmost_(current_->right); // ... goes to successor
+			// Else if it has no successor under itself
+			else if (current_ == current_->parent->left) // If it is its parent's left child
+				current_ = current_->parent; // ... it becomes its parent
+			// Then it's its parent's right child
+			while (current_ == current_->parent->right) // Go up the succession of right children
+				current_ = current_->parent;
+			if (current_ == current_->parent->left) // if it is its parent's left child
+				current_ = current_->parent; // ... it becomes its parent
+			else
+				current_ = NULL; // ..else NULL, end is reached
+			return *this;
+		}
+
+		// const_iterator will cycle backward passing through an end's marker
+		const_iterator &operator--()
+		{
+			if (root_ == NIL)
+				current_ = NULL;
+			else if (current_ == NULL)
+				current_ = rightmost_(root_);
+			// If tree was empty but stuff got in since last call
+			else if (current_->left != NIL)
+				current_ = rightmost_(current_->left);
+			else if (current_->parent && current_ == current_->parent->right)
+				current_ = current_->parent;
+			else if (current_->parent)
+			{
+				while (current_->parent && current_ == current_->parent->left)
+					current_ = current_->parent;
+				current_ = current_->parent && current_ == current_->parent->right ? current_->parent : NULL;
+			}
+			else
+				current_ = NULL;
+			return *this;
+		}
+
+		const_iterator operator++(int)
+		{
+			const_iterator tmp = *this;
+			operator++();
+			return tmp;
+		}
+
+		const_iterator operator--(int)
+		{
+			const_iterator tmp = *this;
+			operator--();
+			return tmp;
+		}
+
+		const mapped_type * operator->() const
+		{
+			return &(current_->value);
+		}
+
+		mapped_type operator*() const
+		{
+			return current_->value;
+		}
+
+		bool operator==(const_iterator const &rhs) const
+		{
+			return current_ == rhs.current_;
+		}
+
+		bool operator!=(const_iterator const &rhs) const
+		{
+			return current_ != rhs.current_;
+		}
+	};
+
+	const_iterator begin() const
+	{
+		return ++const_iterator(root_);
+	}
+
+	const_iterator end() const
+	{
+		return const_iterator(root_);
+	}
 
 	class iterator
 	{
@@ -373,12 +460,9 @@ class AA_tree
 			: root_(other.current_), current_(other.current_)
 		{ }
 
-
-		// Conversion operator
-		// Allows non-const to const implicit conversion !!!
-		operator typename AA_tree<Key, const Value, KeyCmpFn, Alloc>::iterator() const
+		/* Conversion */ operator const_iterator()
 		{
-			return AA_tree<Key, const Value, KeyCmpFn, Alloc>::iterator(root_, current_);
+			return const_iterator(root_, current_);
 		}
 
 		iterator &operator=(iterator const &rhs)
@@ -388,7 +472,7 @@ class AA_tree
 			return *this;
 		}
 
-		// Iterator will cycle forward with passing by an end's marker
+		// Iterator will cycle forward passing through an end's marker
 		iterator &operator++()
 		{
 			if (root_ == NIL) // Tree empty ?
@@ -412,6 +496,7 @@ class AA_tree
 			return *this;
 		}
 
+		// Iterator will cycle backward passing through an end's marker
 		iterator &operator--()
 		{
 			if (root_ == NIL)
@@ -469,16 +554,68 @@ class AA_tree
 		}
 	};
 
-
-	iterator begin() const
+	iterator begin()
 	{
 		return ++iterator(root_);
 	}
 
-	iterator end() const
+	iterator end()
 	{
 		return iterator(root_);
 	}
+
+
+	/* DEBUG */
+
+	void print_node(std::stringstream &ss, node_pointer node)
+	{
+		if (node != NIL)
+		{
+			ss << node->key << " [label=< <b>" << node->key << "</b><br/> <sub>" << node->level << "</sub>>]\n\t";
+			if (node->left != NIL)
+			{
+				if (node->left->level == node->level)
+				{
+					ss << "{rank=same; " << node->key << "; " << node->left->key << "}\n\t";
+					ss << node->left->key << " [color=red]\n\t";
+				}
+				ss << node->key << " -> " << node->left->key << "\n\t";
+				print_node(ss, node->left);
+			}
+			if (node->right != NIL)
+			{
+				if (node->right->level == node->level)
+				{
+					ss << "{rank=same; " << node->key << "; " << node->right->key << "}\n\t";
+					ss << node->right->key << " [color=red]\n\t";
+				}
+				ss << node->key << " -> " << node->right->key << "\n\t";
+				print_node(ss, node->right);
+			}
+		}
+	}
+
+#define DEFAULT_FILENAME "tree"
+	void print_dot()
+	{
+		static int n;
+
+		print_dot(++n);
+	}
+
+	void print_dot(int n)
+	{
+		std::stringstream ss;
+		print_node(ss, root_);
+
+		std::stringstream title;
+		title << DEFAULT_FILENAME << n << ".dot";
+		std::ofstream file(title.str().c_str());
+		file << "digraph {\n\n\t";
+		file << ss.str();
+		file << "\n}";
+	}
+
 
 #undef NIL
 }; // class AA_tree
