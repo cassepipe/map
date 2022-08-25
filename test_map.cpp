09e40080f7b17e5f0147928a34dadf09393943fa
cassepipe <cassepipe@ymail.com>
2022-08-25 18:58:57 +0200

    Passes map_clear

--- test_map.cpp
+++ test_map.cpp
@@ -7,8 +7,7 @@
 int test_map()
 {
 	test_map_begin();
-	/*test( test_map_begin() )*/
-	/*test( test_map_clear() )*/
+	test_map_clear();
 	/*test( test_map_constructor() )*/
 	/*test( test_map_count() )*/
 	/*test( test_map_empty() )*/
@@ -52,6 +51,23 @@ int	test_map_begin()
 
 int	test_map_clear()
 {
+	NAMESPACE::map<char, int> myMap;
+
+	myMap['x']=100;
+	myMap['y']=200;
+	myMap['z']=300;
+
+	std::cout << "myMap contains:" << std::endl;
+	for ( NAMESPACE::map<char, int>::iterator it = myMap.begin(); it != myMap.end(); ++it)
+		std::cout << it->first << "=>" << it->second << std::endl;
+
+	myMap.clear();
+	myMap['a']=1101;
+	myMap['b']=2202;
+
+	std::cout << "myMap contains:" << std::endl;
+	for ( NAMESPACE::map<char, int>::iterator it = myMap.begin(); it != myMap.end(); ++it)
+		std::cout << it->first << "=>" << it->second << std::endl;
 
 	return 0;
 }
